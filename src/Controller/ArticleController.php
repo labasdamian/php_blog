35244429c8cc1ea7509f2dd5b249da851363e060
Damian Łabas <damian.labas@codibly.com>
2018-12-28 11:59:21 +0100

    first commit, article list, show single article

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController{
+
+    /**
+     * @Route("/", name="article_list")
+     * @Method({"GET"})
+     */
+    public function index()
+    {
+        $articles = $this->getDoctrine()->getRepository(Article::class)->findAll();
+
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles
+        ]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="article_show")
+     */
+    public function show($id) {
+        $article = $this->getDoctrine()->getRepository(Article::class)->find($id);
+
+        return $this->render('articles/show.html.twig', [
+            'article' => $article
+        ]);
+    }
+
+    /**
+     * @Route("/article/save")
+     */
+    public function save(){
+        $entityManager  = $this->getDoctrine()->getManager();
+
+        $article = new Article();
+        $article->setTitle('Article Two');
+        $article->setBody('This is the body for article two ');
+
+        $entityManager->persist($article);
+
+        $entityManager->flush();
+
+        return new Response('Save an article with the if of '.$article->getId());
+    }
+}
